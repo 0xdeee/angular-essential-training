471712147000052ef1dcbb30b4d545572531eed7
0xdeee <21086243+0xdeee@users.noreply.github.com>
2021-12-13 23:54:46 +0530

    completed section 2

---
+++ src/app/media-item/media-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media-item',
+  templateUrl: './media-item.component.html',
+  styleUrls: ['./media-item.component.css'],
+})
+export class MediaItemComponent implements OnInit {
+  @Input() mediaItem: any;
+  constructor() {}
+
+  ngOnInit() {}
+}
