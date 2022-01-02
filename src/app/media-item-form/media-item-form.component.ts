37f5ba94a4c30b701d3bf6b64c030623c8c97d76
0xdeee <21086243+0xdeee@users.noreply.github.com>
2022-01-02 13:34:15 +0530

    forms & form validation

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-media-item-form',
@@ -20,10 +20,46 @@ export class MediaItemFormComponent implements OnInit {
      */
     this.form = new FormGroup({
       medium: new FormControl('Movies'),
-      name: new FormControl(''),
+      /**
+       * [10.a]
+       * We can add built-in basic validators to form field using the Validators class
+       * we can also group multiple Validators to same field by using compose () and
+       * passing all validators required in an array
+       * as the use enter the data in these fields, angular will validate it and add ng-invalid class
+       * to the markup (which can be used to display feedback to user)
+       * by default, the form will be submittable even if invalid. so we have to explicitly -
+       * valid property in form object and disable submit button if the form is not valid
+       */
+      name: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('[\\w\\-\\s\\/]+'),
+        ])
+      ),
       category: new FormControl(''),
-      year: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
     });
+  }
+
+  /**
+   * [10.b]
+   * We can build custom validator functions that return null || object based on required validation
+   * here im considering it valid if year is empty or year in exclusive range of 1900 to 2100
+   * and considering it invalid if otherwise
+   * we just have to pass a reference to this method in the form field's FormControl Object
+   * all other working like, applying ng-valid & ng-invalid class will be handled by angular
+   */
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    if (year <= 1900 || year >= 2100) {
+      return { year: true };
+    } else {
+      return null;
+    }
   }
 
   onSubmit(formData) {
